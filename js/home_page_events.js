3a99d8b36b4f4fcd96e187c6c1aa6ea8ea164d45
Elliott Barringer <elliottbarringer@icloud.org>
2023-11-26 18:07:03 -0800

    push2
    began back and front end for the welcome/home page that users initially see on open

---
+++ js/home_page_events.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const p1 = document.getElementById('p1');
+    const start_button = document.getElementById('start_b');
+
+    start_button.addEventListener('click', () => {
+        
+    })
+});

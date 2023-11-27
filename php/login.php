3453210c94f76b209689bad09d1b33eeac2d1ec4
Elliott Barringer <elliottbarringer@icloud.org>
2023-11-26 19:00:08 -0800

    push 3
    v1 of login php script, checking if works on aayush computer, changes made to html web page for ids and names, and changes made to events.js for page forwarding

---
+++ php/login.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "dummypassword";
+$dbname = "group-project-manager";
+
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->select_db($dbname);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['username'];
+    $pwd = $_POST['pwd'];
+    $insertSQL = "INSERT INTO accounts (username, password) VALUES ('$name', $pwd')";
+
+    if ($conn->query($insertSQL) === TRUE) {
+        echo "New record added successfully";
+    } else {
+        echo "Error: " . $insertSQL . "<br>" . $conn->error;
+    }
+}
+$conn->close();
+?>

5b46095ab633b170a131c0315966fd44a9f0b93b
aayushs-edu <aayushs2008@gmail.com>
2023-12-02 12:53:08 -0800

    Trying to remove duplicate accounts

--- php/make_account.php
+++ php/make_account.php
@@ -18,13 +18,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['username'];
     $raw_pwd = $_POST['pwd'];
     $hashed_pwd = hash('sha256', $raw_pwd); #hash the passwords using sha256 for security
-    $insertSQL = "INSERT INTO accounts (username, email, password) VALUES ('$name', '$email', '$hashed_pwd')";
-
-    if ($conn->query($insertSQL) === TRUE) {
-        echo "New record added successfully";
+    
+    //Check if account with that username already exists
+    $scanSQL = "SELECT * FROM accounts WHERE username = '$name'"; // query here to scan the db for the username and password submitted 
+    $result = $conn->query($scanSQL);
+    
+    if ($result) {
+        if ($result->num_rows > 0) {
+            header("Location: ../make_account_page.html");
+            echo "An account with that username already exists.";
+            die();
+        }
     } else {
-        echo "Error: " . $insertSQL . "<br>" . $conn->error;
+        echo "Error: " . $scanSQL . "<br>" . $conn->error;
     }
+    
+    # $insertSQL = "INSERT INTO accounts (username, email, password) VALUES ('$name', '$email', '$hashed_pwd')";
+
+    # if ($conn->query($insertSQL) === TRUE) {
+    #     echo "New record added successfully";
+    # } else {
+    #     echo "Error: " . $insertSQL . "<br>" . $conn->error;
+    # }
 }
 $conn->close();
 ?>

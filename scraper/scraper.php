40150899861b89d046d13b987b7353ab064e4098
Shaunik Musukula <shaunik.musukula@gmail.com>
2023-12-02 16:59:37 -0800

    Allow canvas API to load courses and due dates with given key

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -1,6 +1,23 @@
 <?php
 
-$api_link = "http://127.0.0.1/scraper/scraper.py?name=John";
-$api_data = file_get_contents($api_link);
+require "vendor/autoload.php";
 
+// load environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+// safely get current url
+if(isset($_SERVER["HTTP"]) && $_SERVER["HTTPS"] === "on")   
+    $url = "https://";   
+else  
+    $url = "http://";
+
+$url .= $_SERVER["HTTP_HOST"]; 
+
+// get API key & base URL
+$api_key = $_ENV["CANVAS_KEY"];
+$base_url = $_ENV["CANVAS_BASE_URL"];
+
+$api_link = $url . "/scraper/scraper.py?url=" . $base_url . "&auth=" . $api_key;
+$api_data = file_get_contents($api_link);
 echo $api_data;
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,14 +2,65 @@
 print("Content-Type: text/html\n")
 import os
 import urllib.parse
+import sys
+import canvasapi
+import datetime
+from pytz import timezone
+import unicodedata
+print("<p>Starting...</p>")
 
-sent_query = os.environ["QUERY_STRING"]
-query_list = sent_query.split("=")
-query_dict = urllib.parse.parse_qs(os.environ["QUERY_STRING"])
+def convert_due_date(assignment):
+    raw_due_date = datetime.datetime.strptime(assignment.due_at,"%Y-%m-%dT%H:%M:%SZ").astimezone(timezone("America/Los_Angeles"))
+    dt = datetime.timedelta(hours=8)
+    return raw_due_date - dt
 
-def greeter(name):
-    print(f"Hello {name}! This is your name!")
+def filter_assignments(course):
+    unfiltered = course.get_assignments(bucket="upcoming")
+    filtered = []
+    for assignment in unfiltered:
+        if datetime.datetime.now() < datetime.datetime.strptime(assignment.due_at,"%Y-%m-%dT%H:%M:%SZ"):
+            filtered.append(assignment)
+    return filtered
 
-input_name = str(query_dict["name"])[2:-2]
+def accept_accents(string):
+    return string.encode("utf-8").decode(sys.stdout.encoding)
 
-print(str(greeter(input_name)))
+try:
+    sent_query = os.environ["QUERY_STRING"]
+    query_list = sent_query.split("=")
+    query_dict = urllib.parse.parse_qs(os.environ["QUERY_STRING"])
+
+    url = query_dict["url"][0]
+    try:
+        auth = query_dict["auth"][0]
+    except KeyError:
+        auth = None
+
+    api_data = canvasapi.Canvas(base_url=f"https://{url}.com", access_token=auth)
+
+    assignment_dict = {}
+
+    for course in api_data.get_courses():
+        try:
+            assignments = filter_assignments(course=course)
+            # print(assignments)
+            if assignments:
+                assignment_dict[course] = assignments
+                # break
+        except:
+            continue
+
+
+    for course, assignments in assignment_dict.items():
+        print(f"<h2>{course}</h2>")
+        for assignment in assignments:
+            if assignment.due_at:
+                due_date = convert_due_date(assignment)
+                print(f"<p>Assignment {accept_accents(assignment.name)}, due on {due_date.strftime("%m/%d/%Y - %I:%M %p")}</p>")
+            else:
+                print(f"<p>Assignment: {accept_accents(assignment.name)}, NO DUE DATE</p>")
+
+except Exception as e:
+    print(f"<h1>ERROR: {e}</h1>")
+
+print("<p>Finished</p>")

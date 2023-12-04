a1d327274f7e37d758e853d9b26434099cc215b0
aayushs-edu <aayushs2008@gmail.com>
2023-12-03 19:11:13 -0800

    FILE LIST SHOWS UP IN PROJECT DASHBOARD
    Each file shows up as a button which downloads the file to user's computer when clicked.

--- php/projects.php
+++ php/projects.php
@@ -1,35 +1,33 @@
 <?php
-    if(isset($_GET['id'])) {
-        session_start();
-        $currentProjectID = $_GET['id'];
+    session_start();
+    $currentProjectID = $_GET['id'];
 
-        $env = parse_ini_file('.env');
-        $servername = $env["SQL_SERVER"];
-        $username = $env["SQL_USERNAME"];
-        $password = $env["SQL_PASSWORD"];
-        $dbname = "group-project-manager";
+    $env = parse_ini_file('.env');
+    $servername = $env["SQL_SERVER"];
+    $username = $env["SQL_USERNAME"];
+    $password = $env["SQL_PASSWORD"];
+    $dbname = "group-project-manager";
 
-        $conn = new mysqli($servername, $username, $password);
-        if ($conn->connect_error) {
-            die("Connection error: " . $conn->connect_error);
-        }
-        $conn->select_db($dbname);
+    $conn = new mysqli($servername, $username, $password);
+    if ($conn->connect_error) {
+        die("Connection error: " . $conn->connect_error);
+    }
+    $conn->select_db($dbname);
 
-        $sessionUser = $_SESSION['user'];
-        $selectFilesSQL = "SELECT fileContent FROM files WHERE projectID = '$currentProjectID'";
-        $result = $conn->query($selectFilesSQL);
-        } else {
-            header('Location: dashboard_page.html');
-        }
+    $sessionUser = $_SESSION['user'];
+    $selectFilesSQL = "SELECT fileID, `fileName`, fileContent FROM files WHERE projectID = '$currentProjectID/'";
+    $result = $conn->query($selectFilesSQL);
+    
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/project_dashboard.css">
     <title>Project Dashboard</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -70,32 +68,26 @@
 <body>
     <div class="container">
         <h1>Project Dashboard</h1>
-        <form action="upload_file.php" method="post" id="uploadForm" enctype="multipart/form-data">
+        <form action="./upload_file.php" method="post" id="uploadForm" enctype="multipart/form-data">
             <input type="file" name="fileInput" id="fileInput" />
             <?php echo "<input type='hidden' name='id' value={$currentProjectID}/>" ?>
             <input type="submit">Upload</input>
         </form>
-        <?php
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_array($result)) {
-                    echo "<p style='text-decoration: none'>{$row['fileContent']}</p>\n";
-                }
-            }
-        ?>
-        <div id="response"></div>
     </div>
     <div class="dashboard">
         <div class="panel">
             <h2>Files</h2>
             <ul class="file-list">
-                <li>File 1.txt</li>
-                <li>File 2.jpg</li>
-                <li>Folder 1
-                    <ul>
-                        <li>Subfile 1.txt</li>
-                        <li>Subfile 2.jpg</li>
-                    </ul>
-                </li>
+                <?php
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_array($result)) {
+                            if(!file_exists('./Files')) mkdir('./Files');
+                            $filePath = './Files/' . $row['fileName'];
+                            if(!file_exists($filePath)) file_put_contents($filePath, $row['fileContent']);
+                            echo "<button class=\"btn btn-primary\" onclick=\"location.href='Files/{$row['fileName']}';\">{$row['fileName']}</button>\n";
+                        }
+                    }
+                ?>
             </ul>
         </div>
 
@@ -117,5 +109,8 @@
             </ul>
         </div>
     </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
--- php/upload_file.php
+++ php/upload_file.php
@@ -1,43 +1,43 @@
 <?php
-session_start();
+    session_start();
 
-$currentProjectID = $_POST['id'];
+    $currentProjectID = $_POST['id'];
 
-function generateRandomHex() {
-    $length = 32; // 64 char, each char => 4 bit
-    $randBytes = random_bytes($length / 2);
-    $hexString = bin2hex($randBytes);
-    return $hexString;
-}
+    function generateRandomHex() {
+        $length = 32; // 64 char, each char => 4 bit
+        $randBytes = random_bytes($length / 2);
+        $hexString = bin2hex($randBytes);
+        return $hexString;
+    }
 
-$rawFileID = generateRandomHex();
-$hashedFileID = hash('sha256', $rawFileID);
+    $rawFileID = generateRandomHex();
+    $hashedFileID = hash('sha256', $rawFileID);
 
-$env = parse_ini_file('.env');
-$servername = $env["SQL_SERVER"];
-$username = $env["SQL_USERNAME"];
-$password = $env["SQL_PASSWORD"];
-$dbname = "group-project-manager";
+    $env = parse_ini_file('.env');
+    $servername = $env["SQL_SERVER"];
+    $username = $env["SQL_USERNAME"];
+    $password = $env["SQL_PASSWORD"];
+    $dbname = "group-project-manager";
 
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$conn->select_db($dbname);
+    $conn = new mysqli($servername, $username, $password);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $conn->select_db($dbname);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fileName = $_FILES['fileInput']['name'];
-    $fileTmpName = $_FILES['fileInput']['tmp_name'];
-    $fileContent =  file_get_contents($fileTmpName);
-    $fileSize = $_FILES['fileInput']['size'];
-    $sql = "INSERT into files (fileID, `fileName`, fileContent, projectID) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ssbs", $hashedFileID, $fileName, $fileContent, $currentProjectID);
-    $stmt->send_long_data(2, file_get_contents($fileTmpName)); 
-    $stmt->execute();
-    echo "File successfully inserted";
-    
-}
-$conn->close();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $fileName = $_FILES['fileInput']['name'];
+        $fileTmpName = $_FILES['fileInput']['tmp_name'];
+        $fileContent =  file_get_contents($fileTmpName);
+        $fileSize = $_FILES['fileInput']['size'];
+        $sql = "INSERT into files (fileID, `fileName`, fileContent, projectID) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ssbs", $hashedFileID, $fileName, $fileContent, $currentProjectID);
+        $stmt->send_long_data(2, file_get_contents($fileTmpName)); 
+        $stmt->execute();
+        echo "File successfully inserted";
+        
+    }
+    $conn->close();
 
 ?>

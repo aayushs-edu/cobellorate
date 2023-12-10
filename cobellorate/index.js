323c1480b14ca4c75d77ef22cbe8c3ab5c927ab7
aayushs-edu <aayushs2008@gmail.com>
2023-12-10 13:42:52 -0800

    Fixed sql

--- cobellorate/index.js
+++ cobellorate/index.js
@@ -18,17 +18,11 @@ app.use(express.urlencoded({
     extended: true,
 }));
 
-<<<<<<< Updated upstream
 app.use(cors());
 
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
-=======
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/js', express.static(__dirname + 'public/js'))
-app.use('/img', express.static(__dirname + 'public/img'))
->>>>>>> Stashed changes
 
 app.set('views', './views')
 app.set('view engine', 'ejs');
@@ -39,59 +33,8 @@ app.use('/users', userRoute);
 app.use(cors());
 
 app.get('/', (req, res) => {
-<<<<<<< Updated upstream
-    res.render('home_page');
-});
-
-app.get('/make_account_page.ejs', (req, res) => {
-    res.render('make_account_page');
-});
-// sign up handling
-app.post('/signup', (req, res) => {
-
-    // from the forms
-    const email = req.body.email;
-    const name = req.body.username;
-    const rawPwd = req.body.pwd;
-
-    const connection = mysql.createConnection({
-        host: servername,
-        username: username,
-        password: password
-    });
-    connection.connect(function (err) {
-        if (err) {
-            console.log('error connecting to sql database: ' + err.stack);
-            return;
-        }
-        console.log('connected to database as id ' + connection.threadId); 
-    });
-    connection.query('USE ' + dbname);
-
-    // for hashing the pwd
-    function generateRandomHex() {
-        const length = 32;
-        const randBytes = crypto.randomBytes(length / 2);
-        const hexString = randBytes.toString('hex');
-        return hexString;
-    }
-    // hash the pwd
-    const hashedPwd = crypto.createHash('sha256').update(rawPwd).digest('hex');
-    // sql query
-    const scanSQL = `SELECT * FROM accounts WHERE username = '${name}'`;
-    connection.query(scanSQL, function (err, result) {
-        if (err) {
-            console.error('error executing query: ' + err.stack);
-            res.send('error executing query');
-            return;
-        }
-        res.send('new record added succesfully');
-    });
-});
-=======
     res.render('home');
-})
->>>>>>> Stashed changes
+});
 
 app.listen(port, () => {
     console.info(`Listening on port ${port}`);
--- cobellorate/routes/users.js
+++ cobellorate/routes/users.js
@@ -1,12 +1,57 @@
 const express = require('express');
+const mysql = require('mysql2');
+const crypto = require('crypto');
 const router = express.Router();
+
+const env = require('dotenv').config().parsed;
+const servername = env.SQL_SERVER;
+const username = env.SQL_USERNAME;
+const password = env.SQL_PASSWORD;
+const dbname = env.SQL_DB;
 
 router.get('/signup', (req, res) => {
   res.render('signup');
 })
 
 router.post('/signup', (req, res) => {
-  res.send(`Your username is ${req.body.username} and your password is ${req.body.pwd}`);
+  // from the forms
+  const email = req.body.email;
+  const name = req.body.username;
+  const rawPwd = req.body.pwd;
+
+  const connection = mysql.createConnection({
+      host: servername,
+      user: username,
+      password: password,
+      database: dbname
+  });
+  connection.connect(function (err) {
+      if (err) {
+          console.log('error connecting to sql database: ' + err.stack);
+          return;
+      }
+      console.log('connected to database as id ' + connection.threadId); 
+  });
+
+  // for hashing the pwd
+  function generateRandomHex() {
+      const length = 32;
+      const randBytes = crypto.randomBytes(length / 2);
+      const hexString = randBytes.toString('hex');
+      return hexString;
+  }
+  // hash the pwd
+  const hashedPwd = crypto.createHash('sha256').update(rawPwd).digest('hex');
+  // sql query
+  const scanSQL = `SELECT * FROM accounts WHERE username = '${name}'`;
+  connection.query(scanSQL, function (err, result) {
+      if (err) {
+          console.error('error executing query: ' + err.stack);
+          res.send('error executing query');
+          return;
+      }
+      res.send('new record added succesfully');
+  });
 })
 
 module.exports = router;

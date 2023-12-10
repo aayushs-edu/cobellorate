8d605ae391300a9884da3a798c44b878c5552719
aayushs-edu <aayushs2008@gmail.com>
2023-12-09 23:21:59 -0800

    node

---
+++ cobellorate_node/index.js
@@ -0,0 +1,17 @@
+// entry point for node
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000; // Set the port
+const DIR = path.join(__dirname, '..')
+
+app.use(express.static(DIR));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(DIR, 'home_page.html'));
+
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

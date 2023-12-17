dd9b1b018460378e30eb0122899d86c6c0d7703c
Elliott Barringer <elliottbarringer@icloud.org>
2023-12-16 23:24:44 -0800

    Revert "refactoring"
    This reverts commit 2426427265a533ea4b63d04fc7ccd4133077d598.

--- js/dashboard_page_events.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const create_project_button = document.getElementById('new-project-button');
-
-    create_project_button.addEventListener('click', () => {
-        window.location.href = 'project_creation_page.html';
-    });
-});
